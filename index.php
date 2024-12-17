c10cad4e4d2ba2c369a0d879c8770d73f6079ca3
JACQUE  HERNANDES <ti.jacque@gmail.com>
2024-12-16 21:10:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// Exibe uma mensagem simples para validar o ambiente
+echo "<h1>Hello, Elastic Beanstalk! 🚀</h1>";
+echo "<p>Welcome to your PHP application deployed with Elastic Beanstalk.</p>";
+
+// Informações do servidor para testes
+echo "<p>Server Info:</p>";
+echo "<pre>";
+print_r($_SERVER);
+echo "</pre>";
+?>
